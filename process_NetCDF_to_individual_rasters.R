df1a8f71f61c8fc94bbc7e89107c49a234ed2c78
Michael Hyslop <mdhyslop@mtu.edu>
2017-11-29 11:38:54 -0500

    edits/updates

--- process_NetCDF_to_individual_rasters.R
+++ process_NetCDF_to_individual_rasters.R
@@ -15,12 +15,15 @@ library(rgdal)
 #check and set working directory - change to your environment
 getwd()
 setwd("I:/ConvertNetCDFToRasters")
+
 #load NetCDF file name into variable object. Modify for additional files 
 #or add loop to process a folder of files. NetCDF File name goes in "" and must be in working directory
 #or full path needs to be supplied
 kt <- "kt.nc"
+
 #read header file of NetCDF file
 nc_open(kt)
+
 #load Precip variable into a RasterBrick object
 bkt <- brick(kt, varname="Prec")
 #list slice names in file
